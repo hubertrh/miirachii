97d5e02ef9c49145a0bcba537a8a2b5aa66b17ef
Hubert Rogala-Haracz <hubert.haracz@gmail.com>
2024-05-27 03:44:22 +0100

    Refactor CSS and Tailwind configuration
    - Update global CSS styles, replacing root variables with layer base and applying new styles to headings and paragraphs
    - Simplify content paths in Tailwind configuration file
    - Extend theme in Tailwind config with additional color options, height properties, and fontFamily settings
    - Enable future-proofing feature for hover effects in Tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,8 @@
 import type { Config } from "tailwindcss";
+const { fontFamily } = require("tailwindcss/defaultTheme");
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
       backgroundImage: {
@@ -13,8 +10,34 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        accent: {
+          primary: {
+            // DEFAULT: "#397B7E",
+            // dark: "#30696B",
+          },
+          secondary: {
+            // DEFAULT: "#D19130",
+            // dark: "#BA7F27",
+          },
+        },
+      },
+      height: {
+        dvh: "100dvh",
+      },
+      minHeight: {
+        dvh: "100dvh",
+      },
+      fontFamily: {
+        // ubuntu: ["var(--font-ubuntu)"],
+        // sans: ["var(--font-sans)", ...fontFamily.sans],
+      },
     },
   },
+  future: {
+    hoverOnlyWhenSupported: true,
+  },
   plugins: [],
-};
+} satisfies Config;
+
 export default config;
