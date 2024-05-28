17084724cb8c0e746b42e96eee38948d1c699231
Hubert Rogala-Haracz <hubert.haracz@gmail.com>
2024-05-28 01:48:24 +0100

    Refactor Hero component for better styling
    - Replace the import of 'HeroBackground' with camelCase 'heroBackground'
    - Add import for 'logoFull'
    - Modify Image attributes to use inline styles for objectFit
    - Update div structure to include logo image and adjust layout
    - Maintain existing text element within updated div structure

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -1,17 +1,31 @@
 import Image from "next/image";
-import HeroBackground from "/public/hero-bg.jpg";
+import heroBackground from "/public/hero-bg.jpg";
+import logoFull from "/public/logo-full.png";
 
 export default function Hero() {
   return (
     <section className="relative grid h-svh w-full place-items-center">
       <Image
         className="opacity-40"
-        src={HeroBackground}
+        src={heroBackground}
         alt="Hero Background"
-        layout="fill"
-        objectFit="cover"
+        fill
+        style={{
+          objectFit: "cover",
+        }}
       />
-      <div className="z-10">
+      <div className="z-10 flex flex-col gap-8">
+        <div className="relative size-48">
+          <Image
+            src={logoFull}
+            alt="Miirachii Logo"
+            fill
+            style={{
+              objectFit: "contain",
+            }}
+          />
+        </div>
+        <h1 className="text-4xl">Miirachii</h1>
         <h1 className="text-4xl">Miirachii</h1>
       </div>
     </section>
