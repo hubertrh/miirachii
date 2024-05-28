9a0ea857cedb2d361e2454ed0a4be40db2cfcf8f
Hubert Rogala-Haracz <hubert.haracz@gmail.com>
2024-05-28 02:11:09 +0100

    Refactor header logo styling
    - Replace direct Image component with a div wrapper for better control over size and opacity
    - Add 'fill' property to Image component for improved aspect ratio handling
    - Update TODO comment to reflect changes in size and opacity behaviour

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -5,14 +5,17 @@ import logoMinimal from "/public/logo-minimal.png";
 export default function Header() {
   return (
     <header className="fixed top-0 z-10 grid w-full grid-cols-3 px-6">
-      {/* TODO: opacity-100 on scroll down */}
-      <Image
-        className="opacity-0"
-        src={logoMinimal}
-        alt="Miirachii Logo"
-        width={128}
-        height={128}
-      />
+      {/* TODO: opacity-100 and size-32 on scroll down */}
+      <div className="relative size-20 opacity-0">
+        <Image
+          src={logoMinimal}
+          alt="Miirachii Logo"
+          fill
+          style={{
+            objectFit: "contain",
+          }}
+        />
+      </div>
       <Nav />
     </header>
   );
