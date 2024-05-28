36e037990fdea148e554d64b333152a354ac9691
Hubert Rogala-Haracz <hubert.haracz@gmail.com>
2024-05-29 00:43:01 +0100

    Add 'About' section to homepage
    - Introduced a new 'About' component in the home page
    - Added an image for the 'About' section
    - Adjusted heading styles in global CSS for better readability and tracking
    - The 'About' section includes a brief introduction and a call-to-action button

--- src/app/(pages)/(home)/page.tsx
+++ src/app/(pages)/(home)/page.tsx
@@ -1,3 +1,4 @@
+import About from "@/components/home/About";
 import Hero from "@/components/home/Hero";
 
 export default function Home() {
@@ -5,6 +6,7 @@ export default function Home() {
     <>
       <Hero />
       {/* FIXME: replace with actual content */}
+      <About />
       <div className="h-[200vh]" />
     </>
   );
---
+++ src/components/home/About.tsx
@@ -0,0 +1,34 @@
+import Image from "next/image";
+import aboutImage from "/public/about.jpg";
+
+export default function About() {
+  return (
+    <section className="flex h-screen items-center justify-center">
+      <div className="relative size-96">
+        <div className="absolute -left-8 top-8 size-full bg-primary" />
+        <Image
+          src={aboutImage}
+          alt="About Image"
+          fill
+          style={{ objectFit: "cover" }}
+        />
+      </div>
+      <div className="z-10 -ml-16 flex max-w-lg flex-col items-end justify-center gap-2">
+        <h1 className="text-6xl">About Me</h1>
+        <div className="text-pretty text-justify tracking-widest">
+          <p>
+            Welcome to miirachii, where I capture and convey stories with
+            clarity and style. I&apos;m Klaudia, dedicated to bringing out the
+            natural beauty and unique qualities of your brand through expert
+            photography and strategic social media content.
+          </p>
+          <p>
+            I am capturing the natural beauty and convey messages that help
+            brands grow with a calm and inviting presence.
+          </p>
+        </div>
+        <button>See more</button>
+      </div>
+    </section>
+  );
+}
