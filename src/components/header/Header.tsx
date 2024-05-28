c056b93b79f6aff057c08a0bdb77296f12ea2195
Hubert Rogala-Haracz <hubert.haracz@gmail.com>
2024-05-28 03:19:53 +0100

    Remove static logo, fetch dynamically from brand info
    - Deleted the static logo-minimal.png file
    - Updated Header component to fetch brand information asynchronously
    - Replaced static logo source with dynamic URL fetched from brand info
    - Added blur placeholder and data URL for image loading effect

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,15 +1,19 @@
 import Image from "next/image";
+import { getBrandInfo } from "../../../sanity/groqGetters/getBrandInfo";
 import Nav from "./Nav";
-import logoMinimal from "/public/logo-minimal.png";
 
-export default function Header() {
+export default async function Header() {
+  const brandInfo = await getBrandInfo();
+
   return (
     <header className="fixed top-0 z-10 grid w-full grid-cols-3 px-6">
       {/* TODO: opacity-100 and size-32 on scroll down */}
       <div className="relative size-20 opacity-0">
         <Image
-          src={logoMinimal}
+          src={brandInfo.logos.logoMinimal.url}
           alt="Miirachii Logo"
+          placeholder="blur"
+          blurDataURL={brandInfo.logos.logoMinimal.metadata.lqip}
           fill
           style={{
             objectFit: "contain",
