dcc53454c8bf3da73d4092d11c5308a077a1d5fd
Hubert Rogala-Haracz <hubert.haracz@gmail.com>
2024-05-29 17:13:18 +0100

    Refactor home page component imports
    - Adjusted the import path for Services in the Home page
    - Moved a FIXME comment to a different location within the Home function

--- src/app/(pages)/(home)/page.tsx
+++ src/app/(pages)/(home)/page.tsx
@@ -1,14 +1,14 @@
 import About from "@/components/home/About";
 import Hero from "@/components/home/Hero";
-import Services from "@/components/home/Services";
+import Services from "@/components/home/services/Services";
 
 export default function Home() {
   return (
     <>
       <Hero />
-      {/* FIXME: replace with actual content */}
       <About />
       <Services />
+      {/* FIXME: replace with actual content */}
       <div className="h-[200vh]" />
     </>
   );
