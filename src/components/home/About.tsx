bdd9c7333d55ce4b7da29ac8d6a3d30e728fb857
Hubert Rogala-Haracz <hubert.haracz@gmail.com>
2024-05-29 11:38:02 +0100

    Refactor About component to improve modularity
    The About component has been refactored for better code organization and readability. The text content of the About section has been moved into a new, separate component called 'AboutText'. This change enhances the modularity of the code, making it easier to manage and update in future.

--- src/components/home/About.tsx
+++ src/components/home/About.tsx
@@ -1,7 +1,6 @@
 import Image from "next/image";
 import aboutImage from "/public/about.jpg";
-import Link from "next/link";
-import { ChevronRight } from "lucide-react";
+import AboutText from "./AboutText";
 
 export default function About() {
   return (
@@ -15,30 +14,7 @@ export default function About() {
           style={{ objectFit: "cover" }}
         />
       </div>
-      <div className="z-10 -ml-16 flex max-w-lg flex-col items-end justify-center gap-3">
-        <h1 className="text-6xl text-primary transition-all duration-300 hover:text-accent">
-          About Me
-        </h1>
-        <div className="mb-2 text-justify">
-          <p>
-            Welcome to miirachii, where I capture and convey stories with
-            clarity and style. I&apos;m Klaudia, dedicated to bringing out the
-            natural beauty and unique qualities of your brand through expert
-            photography and strategic social media content.
-          </p>
-          <p>
-            I am capturing the natural beauty and convey messages that help
-            brands grow with a calm and inviting presence.
-          </p>
-        </div>
-        <Link
-          href="/about"
-          className="group flex items-center text-lg capitalize tracking-widest underline decoration-transparent underline-offset-4 transition-all duration-300 ease-out hover:text-accent hover:decoration-accent"
-        >
-          <ChevronRight className="mr-2 size-5 transition-all duration-300 ease-out group-hover:translate-x-1 group-hover:text-accent" />
-          Discover my story
-        </Link>
-      </div>
+      <AboutText />
     </section>
   );
 }
