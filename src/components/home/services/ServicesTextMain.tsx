97343121a84586cb60f07bd3487eea3ba8372aaa
Hubert Rogala-Haracz <hubert.haracz@gmail.com>
2024-05-29 23:12:34 +0100

    Refactor ServicesTextMain component layout
    Adjust the layout of the ServicesTextMain component. Decrease gap in flex container, change heading level from h1 to h2, and increase margin-bottom for paragraph text.

--- src/components/home/services/ServicesTextMain.tsx
+++ src/components/home/services/ServicesTextMain.tsx
@@ -1,11 +1,11 @@
 export default function ServicesTextMain() {
   return (
-    <div className="flex min-w-128 flex-col items-start justify-center gap-10">
-      <h1 className="text-6xl text-primary transition-all duration-300 hover:text-accent">
+    <div className="flex min-w-128 flex-col items-start justify-center gap-8">
+      <h2 className="text-6xl text-primary transition-all duration-300 hover:text-accent">
         Services
-      </h1>
+      </h2>
       <div className="text-justify">
-        <p className="mb-3 text-lg">I focus on three pillars:</p>
+        <p className="mb-4 text-lg">I focus on three pillars:</p>
         <div className="ml-20">
           <p className="transition-all duration-300 hover:translate-x-2">
             evocative photography
