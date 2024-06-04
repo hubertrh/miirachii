9f9c4e5c0ec1ac54e426a7a3c6d35671193e090a
Hubert Rogala-Haracz <hubert.haracz@gmail.com>
2024-06-04 12:29:07 +0100

    Add portfolio page
    A new portfolio page has been added to the application. This includes a Hero component with a 'services' variant and a div element for content with minimum screen height.

---
+++ src/app/(pages)/portfolio/page.tsx
@@ -0,0 +1,10 @@
+import Hero from "@/components/home/Hero/Hero";
+
+export default function page() {
+  return (
+    <>
+      <Hero variant="services" />
+      <div className="min-h-screen"></div>
+    </>
+  );
+}
