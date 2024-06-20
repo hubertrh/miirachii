582316a71418e9196ea09f5ddfe24ac22a392c06
Hubert Rogala-Haracz <hubert.haracz@gmail.com>
2024-06-20 08:44:11 +0100

    Add Services page with Hero component
    A new Services page has been added to the application. Currently, it includes a Hero component displaying a placeholder hook. The integration of CMS data and portfolio categories is planned for future updates.

---
+++ src/app/(pages)/services/page.tsx
@@ -0,0 +1,15 @@
+import Hero from "@/components/Hero/Hero";
+
+export default function Services() {
+  // const pageContent = await getServicesPage();
+
+  return (
+    <>
+      {/* TODO: Replace with CMS data */}
+      <Hero hook="My Services, Artfully Captured" />
+      {/* <PortfolioCategories
+        portfolioCategories={pageContent.portfolioCategories}
+      /> */}
+    </>
+  );
+}
